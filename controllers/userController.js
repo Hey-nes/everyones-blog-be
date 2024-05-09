2398d515ad15db60986cba07de9b7cd027da0f6a
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-05-09 16:31:41 +0200

    added login

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,3 +20,33 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+
+    const user = await userModel.findOne({ name });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, isAdmin: user.isAdmin },
+      config.jwtSecret,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error("Login failed:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+// Route for user registration
 router.post("/api/users/register", userController.registerUser);
+
+// Route for user login
+router.post("/api/users/login", userController.loginUser);
 
 module.exports = router;
