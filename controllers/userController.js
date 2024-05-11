60188cbcf1afc64f46cc833ccd511f635cbf23f6
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-05-11 11:17:15 +0200

    added function/route to fetch users

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,3 +62,13 @@ exports.loginUser = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,7 @@ router.post("/api/users/login", userController.loginUser);
 // Route for user registration
 router.post("/api/users/register", userController.registerUser);
 
+// Route to fetch all users
+router.get("/api/users", userController.getUsers)
+
 module.exports = router;
